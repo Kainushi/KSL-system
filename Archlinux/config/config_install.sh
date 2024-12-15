eae4f8ee5ebfead96c5e0cbed7e1790ce07ce8f2
kainushi <kainushi@black-ship.net>
2024-12-15 18:36:48 +0000

    Mise a jour de la config de Sway pour Wofi et mise a jour de la liste des paquets"

--- Archlinux/config/config_install.sh
+++ Archlinux/config/config_install.sh
@@ -1,2 +1,5 @@
 #!/bin/bash/
+# installation de yay
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+# Copy config
 cp -r desktop/* $HOME/.config
